8987f1698c127da652aa9671f5cd342a14d518ef
Ridaa Nasir <ridaanasirr@outlook.com>
2024-07-15 13:59:03 +0300

    working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+//importing icons
+import { GiHamburger } from 'react-icons/gi';
+import { MdOutlineRestaurant } from 'react-icons/md';
 
+import images from '../../constants/images'
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
+  <div className='app__navbar'>
     Navbar
   </div>
 );
