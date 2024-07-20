b3b184aad7239c9a102f21798837517980b535bc
Ridaa Nasir <ridaanasirr@outlook.com>
2024-07-20 18:44:13 +0300

    done with the third container

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-
 import './MenuItem.css';
 
-const MenuItem = ({title, price, tags}) => (
+const MenuItem = ({ title, price, tags }) => (
   <div className='app__menuItem'>
-
     <div className='app__menuItem-head'>
       <div className='app__menuItem-name'>
-        <p className='p__cormorant' style={{color: '#BCCA87'}}>{title}</p>
+        <p className='p__cormorant' style={{ color: '#DCCA87' }}>{title}</p>
       </div>
+      
+      <div className='app__menuItem-dash' />
 
       <div className='app__menuItem-price'>
-        <p className='p__cormorant' >{price}</p>
+        <p className='p__cormorant'>{price}</p>
       </div>
-
-
+    </div>
+    <div className='app__menuItem-sub'>
+      <p className='p__opensans' style={{ color: '#AAA' }}>{tags}</p>
     </div>
   </div>
 );
