1e1d945b7c6a3f85952bbdd864a24070d38f24cf
Ridaa Nasir <ridaanasirr@outlook.com>
2024-07-23 18:17:08 +0300

    done witn changes

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -5,7 +5,7 @@ import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div className='app__bg app__wrapper section__padding'>
+  <div className='app__bg app__wrapper section__padding '>
 
     <div className='app__wrapper_img app__wrapper_img-reverse'>
        <img src={images.chef} alt="Chef image" />
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -4,7 +4,7 @@ import { SubHeading, MenuItem } from '../../components';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div className='app__specialMenu flex__center section__padding'>
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
     <div className='app__specialMenu-title'>
       <SubHeading title="Menu that fits Your Palette" />
       <h1 className='headtext__cormorant'>Today's Special</h1>
